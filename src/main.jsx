ac34f66fc5122e97c03bc7f8698da954fe0d82e1
Ayush Kumar <140801177+ayush-129@users.noreply.github.com>
2024-04-20 17:44:38 +0530

    fixing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from "./layout.jsx";
 import Home from "./components/home/home.jsx";
 import About from "./components/about/about.jsx";
@@ -14,50 +8,20 @@ import Contact from "./components/contact/contact.jsx";
 import User from "./components/user/user.jsx";
 import Leetcode, { leetcodeInfoLoader, leetcodeInfoLoaderForNull } from "./components/leetcode/leetcode.jsx";
 
-// const router = createBrowserRouter([
-//   {
-//     path:'/',
-//     element:<Layout/>,
-//     children:[
-//       {
-//         path:"",
-//         element:<Home/>
-//       },
-//       {
-//         path:"about",
-//         element:<About/>
-//       },
-//       {
-//         path:"contact",
-//         element:<Contact/>
-//       }
-//     ]
-//   }
-// ])
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="user/:userid" element={<User />} />
-      <Route
-        loader={leetcodeInfoLoader}
-        path="leetcode/:userid"
-        element={<Leetcode />}
-      />
-      <Route
-        loader={leetcodeInfoLoaderForNull}
-        path="leetcode"
-        element={<Leetcode />}
-      />
-    </Route>
-  )
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/user/:userid" component={User} />
+          <Route path="/leetcode/:userid" element={<Leetcode />} />
+          <Route path="/leetcode" element={<Leetcode />} />
+        </Switch>
+      </Layout>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
