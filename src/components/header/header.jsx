106dd05e4ac72ccb34111b42a008383e3605cadb
Ayush Kumar <140801177+ayush-129@users.noreply.github.com>
2024-04-20 19:17:12 +0530

    add2

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -79,7 +79,7 @@ export default function Header() {
               <li>
                 <NavLink
                   exact
-                  to="https://ayush-129.github.io/React-Routing/"
+                  to="/React-Routing/"
                   activeClassName="text-orange-700"
                   className="block py-2 pr-4 pl-3 duration-200 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0"
                 >
