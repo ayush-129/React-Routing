9527e2f0f0b22c5c0cc8ee4fae7e22b9ba06b2a7
Ayush Kumar <140801177+ayush-129@users.noreply.github.com>
2024-04-20 17:47:41 +0530

    ll

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./layout.jsx";
 import Home from "./components/home/home.jsx";
 import About from "./components/about/about.jsx";
@@ -8,20 +8,30 @@ import Contact from "./components/contact/contact.jsx";
 import User from "./components/user/user.jsx";
 import Leetcode, { leetcodeInfoLoader, leetcodeInfoLoaderForNull } from "./components/leetcode/leetcode.jsx";
 
+const routes = (
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route
+        loader={leetcodeInfoLoader}
+        path="leetcode/:userid"
+        element={<Leetcode />}
+      />
+      <Route
+        loader={leetcodeInfoLoaderForNull}
+        path="leetcode"
+        element={<Leetcode />}
+      />
+    </Route>
+  </Routes>
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Layout>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/user/:userid" component={User} />
-          <Route path="/leetcode/:userid" element={<Leetcode />} />
-          <Route path="/leetcode" element={<Leetcode />} />
-        </Switch>
-      </Layout>
-    </Router>
+    <Router>{routes}</Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
